9e5340a94bed37cce6591f754866f2f008ea3675
Tanmay Deshmukh <99901740+qwertytanmay@users.noreply.github.com>
2023-09-12 19:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	 0 1 2 3 4 5 6 7 8 l=9
+  for(let i=0; i<arr.length-2; i++){
+	  let sum = arr[i] + arr[i+1] + arr[i+2];
+	  if(sum > target){
+		  return sum;
+	  }
+  }
 }
 
 module.exports = threeSum;
